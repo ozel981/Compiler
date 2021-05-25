498af1669ba1d21f18c4eed6c0de1f6c64771d9f
ozel981 <ozel981@interia.pl>
2021-05-25 12:38:53 +0200

    initial-commit

---
+++ Compilter/Compilter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Compilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
